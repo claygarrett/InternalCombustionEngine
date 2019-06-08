b629b2dadcf5e7a7b6187002d429c79df4172954
Clay Garrett <claygarrett@gmail.com>
2019-06-08 13:32:49 -0400

    Initial Commit

---
+++ CombustibleEngine/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CombustibleEngine
+//
+//  Created by Clay Garrett on 6/8/19.
+//  Copyright © 2019 Clay Garrett. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
